6804c00c054082bb079cd4b302ff23f696405ae8
tanghh0410 <37869339+tanghh0410@users.noreply.github.com>
2020-06-29 09:11:11 +0800

    Add files via upload

---
+++ demo/demo_base/src/test/java/demo/demo_base/DemoBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.demo_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ demo/demo_service/src/test/java/demo/demo_service/DemoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.demo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
